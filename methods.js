e0c7024db8b940152ec857852dc43ff80bf3cc23
DablioZe <wyskzito@gmail.com>
2022-05-22 18:38:01 -0300

    feat: add methods exporter

---
+++ methods.js
@@ -0,0 +1,17 @@
+module.exports = {
+    add: require('./methods/add'),
+    all: require('./methods/all'),
+    concat: require('./methods/concat'),
+    delete: require('./methods/delete'),
+    exists: require('./methods/exists'),
+    get: require('./methods/get'),
+    getDatabases: require('./methods/getDatabases'),
+    has: require('./methods/has'),
+    push: require('./methods/push'),
+    remove: require('./methods/remove'),
+    reset: require('./methods/reset'),
+    set: require('./methods/set'),
+    splice: require('./methods/splice'),
+    sub: require('./methods/sub'),
+    sum: require('./methods/sum')
+}
