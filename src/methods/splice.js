dfe4531e30eba39e788fbe25d3c88c22fd53669c
Wz <63757378+DablioZe@users.noreply.github.com>
2022-06-21 19:27:35 +0000

    fix(splice): add a deep object filter in the splice method
    Added an "Object Finder" in the splice function, thus being able to delete data with only one item
    of that data.

--- src/methods/splice.js
+++ src/methods/splice.js
@@ -1,53 +1,23 @@
-const { get } = require("lodash");
+const { set, get, indexOf, findIndex } = require("lodash");
 const handler = {
     read: require("../handler/read"),
     write: require("../handler/write"),
 };
 
 module.exports = ([identifier, solidData], database) => {
-    const saved_data = handler.read(database);
+    var saved_data = handler.read(database);
     var filtered_data = get(saved_data, identifier, {});
 
     switch (identifier.constructor) {
-        case String:
-        case Number:
+        case String: case Number:
             if (filtered_data.constructor === Array) {
                 switch (solidData.constructor) {
-                    case String:
-                    case Number:
-                        if (filtered_data.indexOf(solidData) >= 0)
-                            filtered_data.splice(
-                                filtered_data.indexOf(solidData),
-                                1
-                            );
-                        break;
-                    case Object:
-                    case Array:
-                        const objectEntries = filtered_data.map((eachObj) =>
-                            typeof eachObj === "object"
-                                ? Object.entries(eachObj)[0]
-                                : eachObj
-                        );
-                        const dataEntries = Object.entries(solidData)[0];
-                        var indexOf = -1;
-
-                        for (
-                            let i = 0;
-                            i < objectEntries.length && indexOf < 0;
-                            i++
-                        ) {
-                            if (typeof objectEntries[i] === "object") {
-                                var [objKey, objValue] = objectEntries[i];
-                                var [dataKey, dataValue] = dataEntries;
-                                if (objKey == dataKey && objValue == dataValue)
-                                    indexOf = i;
-                            }
-                        }
-                        if (indexOf >= 0) filtered_data.splice(indexOf, 1);
-                        break;
+                    case String: case Number: case Boolean: filtered_data.splice(indexOf(filtered_data, solidData), 1); break;
+                    case Object: case Array: filtered_data.splice(findIndex(filtered_data, solidData), 1); break;
                 }
+                set(saved_data, identifier, filtered_data);
             }
-            break;
+        break;
     }
     handler.write(saved_data, database);
     return get(saved_data, identifier, solidData);
