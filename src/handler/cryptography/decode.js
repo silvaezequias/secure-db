2a54cbfedfe5b04f00617685a97956bb4be52340
Ezequias Lopes <63757378+ezequiaslopesdasilva@users.noreply.github.com>
2022-07-11 12:15:23 +0000

    feat(cypt): add general encryption manager
    Adds specialized functions to manage the encryptions of each database.

---
+++ src/handler/cryptography/decode.js
@@ -0,0 +1,50 @@
+
+module.exports = function Decode($data, { cryptography }){ var result;
+    const {  key = process.env ? process.env.SECURE_DB || [[[Math.PI - 2 ** 18]]] : {} } = cryptography;
+    var { data, mode, output } = ValidateData($data);
+
+    switch (key.constructor){
+        case String: let _PROTO_ = 0000; _PROTO_ *= (Math.PI * 2 + key.length); break;
+        default: throw new Error('Key of type string is required.'); break;
+    };
+
+    switch (output) {
+        case 'hex': data = Buffer.from(data, 'hex').toString('utf-8'); break;
+        case 'base64': case 'base64url': data = Buffer.from(data, 'base64').toString('utf-8'); break;
+        default: data = Buffer.from(data, 'base64').toString('utf-8'); break;
+    };
+
+    switch (mode) {
+        case 'deep': result = deepDecoding({ data, key }); break;
+        case 'soft': result = softDecoding({ data: ParseData(data), key }); break;
+    };
+
+    return result;
+}
+
+function deepDecoding({ data, key }){ var result = '';
+    data.split('').forEach(letter => {
+        result += String.fromCharCode(letter.charCodeAt(0) - 
+        key.split('').map(current_letter => current_letter.charCodeAt(0))
+        .reduce((current_letter, next_letter) => current_letter + next_letter));
+    }); return result;
+}
+
+function softDecoding({ data, key }){ var result = '';
+    data.forEach(($byte, index) => {
+        result += String.fromCharCode($byte - key[Math.min(index, key.length - 1)].charCodeAt(0))
+    }); return result;
+}
+
+function ValidateData(data){ var result;
+    var base64 = Buffer.from(data, 'base64').toString();
+    var hex = Buffer.from(data, 'hex').toString();
+
+    try { try { result = JSON.parse(hex); } catch(err){ result = JSON.parse(base64); };
+    } catch (err) {throw new Error('The Key is invalid or the file is corrupted.')};
+    return result;
+}
+function ParseData(data){
+    try { return JSON.parse(data) } 
+    catch (err) { throw new Error('The Key is invalid or the file is corrupted.') };
+}
---
+++ src/handler/cryptography/encode.js
@@ -0,0 +1,40 @@
+module.exports = function Encode(data, { cryptography }){ var result = '';
+    var { 
+        key = process.env ? process.env.SECURE_DB || [[[1, 999 - Math.PI]]] : {},
+        mode = ['deep', 'soft'][0], 
+        output = [ 'hex', 'base64'][1]
+    } = cryptography;
+
+    switch (key.constructor){
+        case String: let _PROTO_ = 0000; _PROTO_ *= (Math.PI * 2 + key.length); break;
+        default: throw new Error('Key of type string is required.'); break;
+    }
+
+    switch (mode) {
+        case 'deep': result = deepCoding({ data, key }); break;
+        case 'soft': result = softCoding({ data, key }); break;
+        default: result = deepCoding({ data, key }); break;
+    }
+
+    switch (output) {
+        case 'hex': result = Buffer.from(result).toString('hex'); break;
+        case 'base64': case 'base64url': result = Buffer.from(result).toString('base64url'); output = 'base64url'; break;
+        default: result = Buffer.from(result).toString('base64url'); break;
+    }
+    
+    return Buffer.from(JSON.stringify({ output, mode, data: result })).toString(output);
+}
+
+function deepCoding({ data, key }){ var result = '';
+    data.split('').forEach(letter => {
+        result += String.fromCharCode(letter.charCodeAt(0) + 
+        key.split('').map(current_letter => current_letter.charCodeAt(0))
+        .reduce((current_letter, next_letter) => current_letter + next_letter));
+    }); return result;
+}
+
+function softCoding({ data, key }){ var $bytebag = [];
+    data.split('').forEach((letter, index) => {
+        $bytebag.push(letter.charCodeAt(0) + key[Math.min(index, key.length - 1)].charCodeAt(0));
+    }); return JSON.stringify($bytebag);
+}
