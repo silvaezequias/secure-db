ec52431cbd053cb7db97aae4a5857af6ff4ead87
Ezequias Lopes <63757378+ezequiaslopesdasilva@users.noreply.github.com>
2022-08-04 17:57:13 +0000

    style: change sharing of data management methods

---
+++ methods.js
@@ -0,0 +1,19 @@
+module.exports = {
+           add: require('./functions/add'),
+           all: require('./functions/all'),
+        concat: require('./functions/concat'),
+        delete: require('./functions/delete'),
+        exists: require('./functions/exists'),
+           get: require('./functions/get'),
+  getDatabases: require('./functions/getDatabases'),
+           has: require('./functions/has'),
+          push: require('./functions/push'),
+        remove: require('./functions/remove'),
+         reset: require('./functions/reset'),
+           set: require('./functions/set'),
+        splice: require('./functions/splice'),
+           sub: require('./functions/sub'),
+           sum: require('./functions/sum'),
+        toggle: require('./functions/toggle'),
+      toObject: require('./functions/toObject')
+};
